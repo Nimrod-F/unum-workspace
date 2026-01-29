f54405871e6429cc754153928c457291200f9833
Nimrod <foldvari_nimrod@yahoo.com>
2026-01-29 21:30:22 +0200

    run hello world function

--- unum-appstore/hello-world/test-local.ps1
+++ unum-appstore/hello-world/test-local.ps1
@@ -5,7 +5,7 @@ Write-Host "Testing Unum Hello-World workflow locally..." -ForegroundColor Green
 
 # Invoke Hello function
 Write-Host "`nInvoking Hello function..." -ForegroundColor Yellow
-sam local invoke HelloFunction -t template.yaml -e test-event.json --region eu-west-1
+sam local invoke HelloFunction -t template.yaml -e test-event.json --region eu-central-1
 
 Write-Host "`nNote: World function is invoked automatically by Hello," -ForegroundColor Cyan
 Write-Host "but SAM local doesn't support Lambda-to-Lambda invocations." -ForegroundColor Cyan
